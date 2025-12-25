dd5099309104bfd81ccb749a36a7e10033d2a089
akgunkaan <kaan1.akgun95@gmail.com>
2025-12-25 01:50:27 +0000

    Deploying to gh-pages from @ akgunkaan/portfolio@fe97aa2107ac9e5e4a675d4b710a2e0634c68211 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/portfolio/";
     },
-  },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/portfolio/repositories/";
-          },
-        },{id: "nav-portfolio",
+  },{id: "nav-portfolio",
           title: "Portfolio",
           description: "AI &amp; Autonomous Systems Portfolio",
           section: "Navigation",
